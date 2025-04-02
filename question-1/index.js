50e9ddcf5f63dc8618f1d6bddd431ab2bde9801f
Utkarsh <utkarshjha.4009@gmail.com>
2025-04-02 19:07:43 +0530

    All functions made

--- question-1/index.js
+++ question-1/index.js
@@ -1,7 +1,25 @@
 const express = require('express')
-const app = express()
-const PORT = 4000
-app.use(express.json())
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+const axios = require('axios')
+const server = express()
+const port = 4000
+const API_BASE = 'http://20.244.56.144/evaluation-service'
+server.use(express.json())
+
+async function getUsersData() {
+  const res = await axios.get(`${API_BASE}/users`)
+  return res.data.users
+}
+
+async function getUserPosts(userId) {
+  const res = await axios.get(`${API_BASE}/users/${userId}/posts`)
+  return res.data.posts || []
+}
+
+async function getCommentsForPost(postId) {
+  const res = await axios.get(`${API_BASE}/posts/${postId}/comments`)
+  return res.data.comments || []
+}
+
+server.listen(port, () => {
+  console.log(`Server up at http://localhost:${port}`)
 })
